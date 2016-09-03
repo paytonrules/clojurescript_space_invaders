9075e64351cbda7a38904a6c58fa3e5d86925fe3
Eric Smith <esmith@8thlight.com>
2016-09-03 14:51:40 -0500

    Show images of the small invader
    - Just a spike, we've got the small invader on screen. One frame (not
      the two). Pixel art should work.
    
      There's a lot here to fix up, I'm gonna add to the Readme.

--- src/space_invaders/core.cljs
+++ src/space_invaders/core.cljs
@@ -14,20 +14,26 @@
 
 (defonce canvas (.getElementById js/document "game"))
 
+(defonce image (js/Image.))
+(set! (.-src image) "images/small-invader.png")
+
 (defn draw-canvas-contents [state ticks]
   (let [ ctx (.getContext canvas "2d")
         w (.-clientWidth canvas)
         h (.-clientHeight canvas)]
     (.clearRect ctx 0 0 w h)
+    (set! (.-fillStyle ctx) "#FFFFFF")
     (doseq [invader-position (view/invaders-to-position (:invaders state) (:ticks state))]
-      (.fillRect ctx
-                 (:x invader-position)
-                 (:y invader-position)
-                 view/invader-width
-                 view/invader-height))))
+      (.drawImage ctx
+                  image
+                  (:x invader-position)
+                  (:y invader-position)))))
 
 (defn draw-game-state [app-state ticks]
   (draw-canvas-contents app-state ticks)
   (js/requestAnimationFrame (partial draw-game-state app-state (inc ticks))))
 
+(set! (.-backgroundColor (.-style canvas)) "black")
+(set! (.-width canvas) 224)
+(set! (.-height canvas) 256)
 (draw-game-state initial-app-state 0)
--- src/space_invaders/view.cljs
+++ src/space_invaders/view.cljs
@@ -2,15 +2,16 @@
 
 (def padding 20)
 (def top 30)
-(def invader-width 20)
+(def invader-width 8)
 (def invader-height 30)
 
-(defn- row-to-position [row-num row]
-  (map-indexed
-    (fn [idx invader]
-      {:x (+ (* invader-width idx) (* (inc idx) padding))
-       :y (+ (* (inc row-num) top) (* invader-height row-num))})
-    row))
-
 (defn invaders-to-position [invaders ticks]
-  (flatten (map-indexed row-to-position invaders)))
+  (-> (map-indexed
+        (fn [row-num row]
+          (map-indexed
+            (fn [idx invader]
+              {:x (+ (* invader-width idx) (* (inc idx) padding))
+               :y (+ (* (inc row-num) top) (* invader-height row-num))})
+            row))
+        invaders)
+      (flatten)))
