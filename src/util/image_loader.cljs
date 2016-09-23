3356af0d9ed7ecae7d7c14aaba85b6670f08a067
Eric Smith <esmith@8thlight.com>
2016-09-23 17:18:19 -0500

    Load image and load images
    With some complicated tests.

--- src/util/image_loader.cljs
+++ src/util/image_loader.cljs
@@ -2,9 +2,31 @@
   (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require [cljs.core.async :as a]))
 
-(defn load-images! [& filenames]
-  (let [chan (a/chan)]
-    (go
-      (a/>! chan "joy"))))
+(defn create-empty-image []
+  (js/Image.))
 
+(defn load-image
+  ([src] (load-image src create-empty-image))
+  ([src create-image]
+   (let [c (a/chan)
+         image (create-image)]
+     (set! (.-src image) src)
+     (set! (.-onload image)
+       (fn []
+         (go (a/>! c image))))
+     c)))
 
+(defn load-images
+  ([image-paths] (load-images image-paths create-empty-image))
+  ([image-paths create-image]
+   (let [complete (cljs.core.async/chan)
+         loading-images (mapv #(load-image % create-image) image-paths)]
+     (if (empty? loading-images)
+       (go (cljs.core.async/>! complete []))
+       (go-loop [images []]
+         (let [[loaded-image _] (a/alts! loading-images)
+               loaded-images (conj images loaded-image)]
+           (if (= (count image-paths) (count loaded-images))
+             (cljs.core.async/>! complete loaded-images)
+             (recur loaded-images)))))
+     complete)))
--- tests/util/image_loader_test.cljs
+++ tests/util/image_loader_test.cljs
@@ -5,14 +5,61 @@
             [devcards.core :refer-macros [deftest]]
             [util.image-loader :as image-loader]))
 
-(deftest load-images
-  (async done
-         (let [chan (image-loader/load-images!)]
-           (go
-             (if (a/<! chan)
-               (done))))))
+(defn create-known-images [images]
+  (let [image (last @images)]
+    (swap! images pop)
+    image))
 
-               ;(is (true? false))
-               ;(is false "error pulling from channel"))
-             ;(done))
-           ;(throw "aw poop"))))
+(defn setup-created-images [image-list]
+  (partial create-known-images (atom image-list)))
+
+(deftest load-image
+  (testing "created image is returned on a channel"
+    (let [fake-image (js-obj)
+          create-image (setup-created-images [fake-image])]
+      (async done
+        (let [chan (image-loader/load-image "source" create-image)]
+          (go
+            (when-let [image (a/<! chan)]
+              (is (re-find #"source$" (.-src image))))
+              (done))
+
+          (.onload fake-image))))))
+
+(deftest load-images
+  (testing "completes with no images"
+    (async done
+      (let [chan (image-loader/load-images '())]
+        (go
+          (when-let [images (a/<! chan)]
+            (is (= [] images) "no images should be loaded")
+            (done))))))
+
+  (testing "completes with one image"
+    (async done
+      (let [fake-image (js-obj)
+            create-image (setup-created-images [fake-image])
+            chan (image-loader/load-images '("first") create-image)]
+        (go
+          (when-let [images (a/<! chan)]
+            (is (= 1 (count images)))
+            (is (= "first" (.-src (first images))))
+            (done)))
+
+        (.onload fake-image))))
+
+  (testing "completes with many images"
+    (let [fake-image-first (js-obj)
+          fake-image-second (js-obj)
+          create-image (setup-created-images [fake-image-second fake-image-first])]
+      (async done
+        (let [chan (image-loader/load-images '("first" "second") create-image)]
+          (go
+            (when-let [images (a/<! chan)]
+              (is (= 2 (count images)))
+              (is (= "first" (.-src (first images))))
+              (is (= "second" (.-src (second images))))
+              (done)))
+
+          (.onload fake-image-first)
+          (.onload fake-image-second))))))
