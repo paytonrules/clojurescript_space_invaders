edca558d618ba071bc24e59087f0f05b5e65a736
Eric Smith <esmith@8thlight.com>
2016-09-29 22:36:24 -0500

    Handle events in the game loop.
    - The code is a bit crude, but you can now add events to the game loop,
      and those events will get called via the signature update(state,
      event). The idea is you'll create a multimethod to handle events and
      states, with a default that's a tick.
    
      The code is a bit cranky, especially in the tests.

--- src/util/game_loop.cljs
+++ src/util/game_loop.cljs
@@ -1,22 +1,48 @@
-(ns util.game-loop)
+(ns util.game-loop
+  (:require [cljs.core.async :as ca])
+  (:require-macros [cljs.core.async.macros :refer [go-loop]]))
 
 (def state (atom nil))
+(def events (atom []))
 
-(defn- update-loop
+(defn clear-events! []
+  (reset! events []))
+
+(defn fire-event! [evt]
+  (swap! events conj evt))
+
+(defn take-event! [cb]
+  (let [evt (first @events)]
+    (swap! events (comp vec rest))
+    (cb evt)))
+
+(defn- update-state! [update-fn event]
+  (update-fn nil event))
+
+(declare update-loop!)
+
+(defn- update-next-frame! [draw update new-state request-next-frame]
+  (reset! state new-state)
+  (draw @state)
+  (request-next-frame #(update-loop! draw update request-next-frame)))
+
+
+(defn- update-loop!
   ([draw update request-next-frame]
-   (let [next-state (update @state)]
+   (let [event-added-state (reduce
+                             (fn [state event]
+                               (update state event))
+                             @state
+                             @events)
+         next-state (update event-added-state :tick)] ; TODO just have a default that is :tick
      (when-not (true? (:quit next-state))
-       (reset! state next-state)
-       (draw @state)
-       (request-next-frame #(update-loop draw update request-next-frame)))))
+       (update-next-frame! draw update next-state request-next-frame))))
   ([draw update initial-state request-next-frame]
-   (reset! state initial-state)
-   (draw @state)
-   (request-next-frame #(update-loop draw update request-next-frame))))
+   (update-next-frame! draw update initial-state request-next-frame)))
 
 (defn start!
   ([options]
    (start! options js/requestAnimationFrame))
 
   ([{:keys [draw update initial-state]} request-next-frame]
-   (update-loop draw update initial-state request-next-frame)))
+   (update-loop! draw update initial-state request-next-frame)))
--- tests/util/game_loop_test.cljs
+++ tests/util/game_loop_test.cljs
@@ -1,6 +1,6 @@
-(ns util.game-loop-test
-  (:require [cljs.test :refer-macros [is testing use-fixtures]]
-            [devcards.core :refer-macros [deftest]]
+(ns ^:figwheel-always util.game-loop-test
+  (:require [cljs.test :refer-macros [async is testing]]
+            [runners.devcards :refer [dev-cards-runner]]
             [util.game-loop :as game-loop]))
 
 (def ^{:private true} requested-frames (atom '()))
@@ -23,11 +23,47 @@
 (defn- reset-requested-frames! []
   (reset! requested-frames '()))
 
+(defn put-one-event []
+  (game-loop/clear-events!)
+  (async done
+    (game-loop/fire-event! {:event :exists})
+    (game-loop/take-event!
+      (fn [evt]
+        (is (= {:event :exists} evt))
+        (done)))))
+
+(defn put-two-events []
+  (game-loop/clear-events!)
+  (async done
+    (game-loop/fire-event! {:event :exists})
+    (game-loop/fire-event! {:also :exists})
+    (game-loop/take-event!
+      (fn [evt]
+        (is (= {:event :exists} evt))
+        (game-loop/take-event!
+          (fn [evt]
+            (is (= {:also :exists} evt))
+            (done)))))))
+
+(defn game-loop-can-be-started-and-stopped []
+  (reset-requested-frames!)
+
+  (game-loop/start!
+    {:draw draw
+     :update (fn [state event] {:quit true})
+     :state {}} request-next-frame)
+
+  (is (= 1 (frames-requested)))
+
+  (call-next-frame!)
+
+  (is (= 0 (frames-requested))))
+
 (defn start-and-update []
   (testing "starts with an initial game state"
     (game-loop/start!
       {:draw draw
-       :update (fn [state] {:new :state})
+       :update (fn [state event] {:quit true})
        :initial-state {:initial :state}}
       request-next-frame)
 
@@ -36,7 +72,7 @@
   (testing "draws the initial state"
     (game-loop/start!
       {:draw draw
-       :update (fn [state] {:new :state})
+       :update (fn [state event] {:quit true})
        :initial-state {:initial :state}}
       request-next-frame)
 
@@ -45,12 +81,16 @@
   (testing "updates the state on the next frame, based on the previous state"
     (let [old-state (atom nil)]
       (reset-requested-frames!)
+      (game-loop/clear-events!)
 
       (game-loop/start!
         {:draw draw
-         :update (fn [state]
-                   (reset! old-state state)
-                   {:new :state})
+         :update (fn [state event]
+                   (if (= state {:new :state})
+                     {:quit true}
+                     (do
+                       (reset! old-state state)
+                       {:new :state})))
          :initial-state {:initial :state}}
         request-next-frame)
 
@@ -61,11 +101,27 @@
       (is (= {:initial :state} @old-state))
       (is (= {:new :state} @game-loop/state))))
 
+  (testing "treats each update as a tick event"
+    (let [event (atom nil)]
+      (reset-requested-frames!)
+
+      (game-loop/start!
+        {:draw draw
+         :update (fn [state e] (reset! event e) {:quit true})
+         :initial-state {:initial :state}}
+        request-next-frame)
+      (call-next-frame!)
+
+      (is (= :tick @event))))
+
   (testing "draws the updated state"
     (reset-requested-frames!)
 
     (let [last-update (atom nil)
-          update (fn [state] {:second :state})]
+          update (fn [state event]
+                   (if (= state {:second :state})
+                     {:quit true}
+                     {:second :state}))]
 
       (game-loop/start!
         {:draw draw
@@ -76,23 +132,36 @@
 
       (is (= {:second :state} @drawn-state)))))
 
-(defn start-will-quit []
-  (reset-requested-frames!)
+(defn process-events []
+  (testing "an event updates state before the tick"
+    (game-loop/clear-events!)
+    (let [update (fn [state event]
+                   (if (= event :testing-event)
+                     :event-based-state
+                     (is (= :event-based-state state))))]
 
-  (testing "do not request the next frame when update returns quit"
-    (game-loop/start!
-      {:draw draw
-       :update (fn [] {:quit true})
-       :state {}} request-next-frame)
+      (game-loop/fire-event! :testing-event)
+      (game-loop/start!
+        {:draw draw
+         :update update
+         :state {}}
+        request-next-frame)
+      (call-next-frame!)))
 
-    (is (= 1 (frames-requested)))
+  (testing "all events are processed before the tick"
+    (game-loop/clear-events!)
+    (let [update (fn [state event]
+                   (if (= event :testing-event-two)
+                     :event-based-state
+                     (when-not (= event :testing-event)
+                       (is (= :event-based-state state)))))]
 
-    (call-next-frame!)
-
-    (is (= 0 (frames-requested)))))
-
-(deftest game-loop-tests
-  "# start-will-quit"
-  (start-will-quit)
-  "# start-and-update"
-  (start-and-update))
+      (game-loop/fire-event! :testing-event)
+      (game-loop/fire-event! :testing-event-two)
+      (game-loop/start!
+        {:draw draw
+         :update update
+         :state {}}
+        request-next-frame)
+      (call-next-frame!))))
+(dev-cards-runner #"util.game-loop-test")
